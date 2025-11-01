d66e94455bf69010dac971168c20a492e3783c04
Ajilx3 <ajilsaji0299@gmail.com>
2025-11-01 18:34:16 +0530

    set up the splashscreeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:terra_scope_apk/Screens/home_screen.dart';
+import 'dart:async';
 
 void main() {
   runApp(const TerraScopeApp());
@@ -12,12 +13,52 @@ class TerraScopeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'terra scope',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,scaffoldBackgroundColor: Colors.blueGrey.shade100,
+      home: const SplashScreen(),
+    );
+  }
+}
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            const SizedBox(height: 20),
+            const Text(
+              'terrascope',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+              ),
+            ),
+          ],
+        ),
       ),
-      home: const HomeScreen(),
     );
   }
 }
