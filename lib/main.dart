5b4d3a4916bdf8ea61d072276a7b3d0e4bfc293c
Ajilx3 <ajilsaji0299@gmail.com>
2025-11-15 14:07:26 +0530

    seperated splashscreen from  the main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:terra_scope_apk/Services/notification_service.dart';
 import 'firebase_options.dart';
-import 'package:terra_scope_apk/Screens/main_page.dart';
+
+import 'Screens/splash_screen.dart';
 import 'dart:async';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // ✅ Get device FCM token BEFORE runApp()
   String? token = await NotificationService.getDeviceToken();
-  print("✅ FCM Device Token: $token");
+  print("FCM device token :$token");
 
   runApp(const TerraScopeApp());
 }
@@ -27,53 +24,6 @@ class TerraScopeApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    // ✅ Navigate after 3 seconds
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MainPage()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('lib/assets/logo.png', height: 150),
-            const SizedBox(height: 20),
-            const Text(
-              'TERRASCOPE',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.15,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
