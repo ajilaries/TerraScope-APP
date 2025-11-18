419abec248db4d8ae7ef13d72298cec95e2936bf
Ajilx3 <ajilsaji0299@gmail.com>
2025-11-18 17:55:28 +0530

    this code determines and store the mode or the user

--- lib/providers/mode_provider.dart
+++ lib/providers/mode_provider.dart
@@ -1,12 +1,20 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ModeProvider extends ChangeNotifier {
-  String _mode = "normal"; // default mode
-
+  String _mode = "default";
   String get mode => _mode;
 
-  void setMode(String newMode) {
+  void setMode(String newMode) async {
     _mode = newMode;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user_mode', newMode);
+  }
+
+  Future<void> loadMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    _mode = prefs.getString('user_mode') ?? "default";
     notifyListeners();
   }
 }
