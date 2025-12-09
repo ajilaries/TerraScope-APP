e23a71def5533d1f41803e56c496117c7462c92c
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-09 12:25:54 +0530

    updated the screen navigation for the major three default screens

--- lib/Screens/main_page.dart
+++ lib/Screens/main_page.dart
@@ -3,7 +3,7 @@ import 'home_screen.dart';
 import 'home_screen2.dart';
 import 'home_screen0.dart';
 import '../Screens/farmer/farmer_dashboard.dart';
-import 'splash_screen.dart';
+// import 'splash_screen.dart';
 // import '../pages/ai_predict_page.dart';
 
 class MainPage extends StatelessWidget {
