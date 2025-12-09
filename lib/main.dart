467bda10650c2306cdd17d542597121cb8830ca0
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-09 12:24:45 +0530

    added proper navigation from the splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:terra_scope_apk/Screens/main_page.dart';
+// import 'package:terra_scope_apk/Screens/main_page.dart';
 import 'package:terra_scope_apk/Screens/splash_screen.dart';
 import 'firebase_options.dart';
 
