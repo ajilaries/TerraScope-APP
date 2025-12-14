929633ef4c75d9c1c84deb46126e488993f44487
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-14 10:45:39 +0530

    updated the api service code with the arguments that are needed for the better and the working of the api service

--- lib/Services/farmer_api_service.dart
+++ lib/Services/farmer_api_service.dart
@@ -2,27 +2,31 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class FarmerApiService {
-  static const String baseUrl = "http://10.0.2.2:8000";
-
   static Future<Map<String, dynamic>> getPrediction({
     required double lat,
     required double lon,
     required String soilType,
   }) async {
-    final response = await http.post(
-      Uri.parse("$baseUrl/ai/predict"),
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({
-        "lat": lat,
-        "lon": lon,
-        "soil_type": soilType,
-      }),
-    );
+    final body = {
+      "year": DateTime.now().year,
+      "lat": lat,
+      "lon": lon,
+      "avg_temp": 28.0,
+      "rainfall_mm": 220.3,
+      "humidity": 90.4,
+      "soil_moisture": 0.54,
+      "crop_grown": soilType,
+    };
 
+    final response = await http.post(
+      Uri.parse("http://10.0.2.2:8000/agri/planting-advice"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(body),
+    );
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception("Failed to fetch prediction");
+      throw Exception("Failed to fetch predictions");
     }
   }
 }
