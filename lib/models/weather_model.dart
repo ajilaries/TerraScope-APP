a08ea29d4c7dc8fda716045a55ebfd55aa1a43c1
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-16 23:54:21 +0530

    updated the weather data from the json file

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,17 +1,25 @@
-class WeatherModel {
-  final double temp;
-  final int humidity;
-  final double wind;
-  final int rainChance;
+class WeatherData {
+  final double temperature;
+  final double rainfall;
+  final double humidity;
+  final double windSpeed;
   final String condition;
-  final String location;
 
-  WeatherModel({
-    required this.temp,
+  WeatherData({
+    required this.temperature,
+    required this.rainfall,
     required this.humidity,
-    required this.wind,
-    required this.rainChance,
+    required this.windSpeed,
     required this.condition,
-    required this.location,
   });
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
+      temperature: (json["temperature"] ?? 0).toDouble(),
+      rainfall: (json["rainfall"] ?? 0).toDouble(),
+      humidity: (json["humidity"] ?? 50).toDouble(),
+      windSpeed: (json["wind_speed"] ?? 3.5).toDouble(),
+      condition: json["condition"] ?? "Unknown",
+    );
+  }
 }
