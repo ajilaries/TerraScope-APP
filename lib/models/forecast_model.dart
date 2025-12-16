39baa71a29f2cfb408ad15a9271530e5162724d8
Ajilx3 <ajilsaji0299@gmail.com>
2025-12-16 23:41:56 +0530

    added the forecast model for the better weather forecast

---
+++ lib/models/forecast_model.dart
@@ -0,0 +1,11 @@
+class ForecastModel {
+  final String day;
+  final double temp;
+  final int rainChance;
+
+  ForecastModel({
+    required this.day,
+    required this.temp,
+    required this.rainChance,
+  });
+}
