85770d460a9733b728f0aea0b725f9ea2b0ce15a
Aju <ajilaries20@gmail.com>
2026-01-04 13:02:35 +0530

    emergency contact file has updated

--- lib/models/emergency_contact.dart
+++ lib/models/emergency_contact.dart
@@ -7,6 +7,7 @@ class EmergencyContact {
   final String email;
   final EmergencyContactType type;
   final String? notes;
+  bool isPrimary;
 
   EmergencyContact({
     required this.id,
@@ -15,6 +16,7 @@ class EmergencyContact {
     required this.email,
     required this.type,
     this.notes,
+    this.isPrimary = false,
   });
 
   String get icon {
